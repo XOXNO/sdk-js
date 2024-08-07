3ff0feb5f40a1116a607522b1c839fe5a07d76d1
Boris Pöhland <me@boris.app>
2024-08-07 10:54:39 +0200

    no export default

--- src/email.tsx
+++ src/email.tsx
@@ -2,4 +2,4 @@ import { renderEmail } from './email/email';
 import { renderEventEmail } from './email/event-email';
 import { renderPostEmail } from './email/post-email';
 export * from './email/types';
-export default { renderEmail, renderEventEmail, renderPostEmail };
+export { renderEmail, renderEventEmail, renderPostEmail };
